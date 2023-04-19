28041c513a5fda1aefc10e84110d1ef0570c8364
Vidya N <131223677+22ds1000017@users.noreply.github.com>
2023-04-19 19:36:06 +0530

    Update and rename test2.py to test3.py

--- test3.py
+++ test3.py
@@ -7,14 +7,12 @@ This app computes the largest of threee numbers.
 TDS - Graded Assignment 8
 """)
 
-number1 = st.number_input('Enter the first number')
-st.write('The first number is ', number1)
+for i in range(0,3,1)
+   number = st.number_input('Enter the first number')
+   number+str(i)= number 
+   st.write('The first number is ', number+str(i))
 
-number2 = st.slider('Enter the second number', 0, 2000, 1)
-st.write("second number", number2)
 
-number3 = st.slider('Enter the third  number', 0,10000, 1)
-st.write("third number ", number3)
 
 if (number2 >= number2) and (number1 >= number3):
    largest = number1
