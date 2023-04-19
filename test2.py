5920c31bb48c386de3eda2a4b7a7260e1932daa6
Vidya N <131223677+22ds1000017@users.noreply.github.com>
2023-04-19 12:28:48 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,24 +1,23 @@
+import streamlit as st
 
-    import streamlit as st
+number1 = st.number_input('Enter the first number')
+st.write('The first number is ', number1)
 
-    number1 = st.number_input('Enter the first number')
-    st.write('The first number is ', number1)
+number2 = st.slider('Enter the second number', 0, 2000, 1)
+st.write("second number", number2)
 
-    number2 = st.slider('Enter the second number', 0, 2000, 1)
-    st.write("second number", number2)
+@st.cache(allow_output_mutation=True)
+def Nums():
+    return []
 
-    @st.cache(allow_output_mutation=True)
-    def Nums():
-        return []
+nums = Nums()
+num = st.sidebar.number_input("Input Number")
+if st.sidebar.button("Add number"):
+    nums.append(num)
 
-    nums = Nums()
-    num = st.sidebar.number_input("Input Number")
-    if st.sidebar.button("Add number"):
-        nums.append(num)
-
-    try:
-        inputs = nums
-        st.table(inputs)
-        st.write("Sum: ", sum(inputs))
-    except:
-        st.title("Enter some numbers")
+try:
+    inputs = nums
+    st.table(inputs)
+    st.write("Sum: ", sum(inputs))
+except:
+    st.title("Enter some numbers")
