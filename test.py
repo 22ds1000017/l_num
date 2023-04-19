8184816a78ec5799b4b1884e1ad3b7c6edacf851
Vidya N <131223677+22ds1000017@users.noreply.github.com>
2023-04-19 11:48:42 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,26 @@
 
 import streamlit as st
 
-number = st.number_input('Insert a number')
-st.write('The current number is ', number)
+number1 = st.number_input('Insert a number')
+st.write('The current number is ', number1)
+
+number2 = st.number_input('Insert a number')
+st.write('The current number is ', number2)
+
+
+number3 = st.number_input('Insert a number')
+st.write('The current number is ', number3)
+
+
+if (number1 >= number2) and (number1 >= number3):
+   largest = number1
+elif (number2 >= number1) and (number2 >= number3):
+   largest = number2
+else:
+   largest = number3
+
+
+#displaying the largest number
+
+st.write("The largest number is", largest)
 
