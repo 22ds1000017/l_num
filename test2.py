cf3632f34033b041471fb06b5f8fa35808f7f62d
Vidya N <131223677+22ds1000017@users.noreply.github.com>
2023-04-19 20:11:12 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -9,11 +9,11 @@ TDS - Graded Assignment 8
 
 
 number1 = st.number_input('Enter the first number')
-st.write("1.", number1)
+st.write("1st number", number1)
 number2 = st.number_input('Enter the second number')
-st.write("2.", number2)
+st.write("2nd number", number2)
 number3 = st.number_input('Enter the third number')
-st.write("3.", number3)
+st.write("3rd number", number3)
 
 if (number1 >= number2) and (number1 >= number3):
    largest = number1
