809ba238676f9c9e7935baac510552906c27544c
Vidya N <131223677+22ds1000017@users.noreply.github.com>
2023-04-19 19:49:46 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -7,19 +7,21 @@ This app computes the largest of threee numbers.
 TDS - Graded Assignment 8
 """)
 
+number_list = []
 for i in range(0,3,1):
-   number = st.number_input('Enter the first number')
-   number+str(i)= number 
-   st.write('The first number is ', number+str(i))
+   number_in = st.number_input('Enter the first number')
+   number_list.append(number_in)
+   st.write('The first number is ', number_list(i))
 
 
 
-if (number2 >= number2) and (number1 >= number3):
-   largest = number1
-elif (number2 >= number1) and (number2 >= number3):
-   largest = number2
+if (number_list[1] >= number_list[2]) and (number_list[1] >= number_list[3]):
+   largest = number_list[1]
+elif (number_list[2] >= number_list[1]) and (number_list[2] >= number_list[3]):
+   largest = number_list[2]
 else:
-   largest = number3
+   largest = number_list[3]
+
 
 
 #displaying the largest number
